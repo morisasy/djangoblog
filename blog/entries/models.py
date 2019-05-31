3a3246ff1ec8ea321ed079c044cc34c482ae5cec
Medi Risasi <morisasy@gmail.com>
2019-05-31 16:40:55 +0300

    view and model added

--- blog/entries/models.py
+++ blog/entries/models.py
@@ -10,3 +10,6 @@ class Entry(models.Model):
 
     class Meta:
         verbose_name_plural = "entries"
+
+    def __str__(self):
+        return f'{self.entry_title}'
--- blog/entries/views.py
+++ blog/entries/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Entry
 
-# Create your views here.
+class HomeView(ListView):
+    model = Entry
+    template_name = "entries/index.html"
