70c92ecedc790efac99f1955f8d63f1613db6d19
Medi Risasi <morisasy@gmail.com>
2019-05-31 14:50:15 +0300

    Meta class admin.py

--- blog/entries/models.py
+++ blog/entries/models.py
@@ -7,3 +7,6 @@ class Entry(models.Model):
     entry_text = models.TextField()
     entry_date = models.DateTimeField(auto_now_add=True)
     entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "entries"
