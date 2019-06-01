0e95604d53fba732237331a110e36981f485077f
Medi Risasi <morisasy@gmail.com>
2019-06-01 22:55:34 +0300

    final v1

--- blog/entries/urls.py
+++ blog/entries/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('entry/<int:pk>/', EntryView.as_view(), name = "entry-detail"),
     path('create_entry/', CreateEntryView.as_view(success_url="/"), name = "create_entry")
     
-
-
 ]
